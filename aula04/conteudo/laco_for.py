2d73bece67d122a1c1b5f6f99581b89aa79a998b
Gyslane Anunciação de Oliveira <gyslane.oliveira@websis.com.br>
2025-06-02 22:05:39 -0300

    Atividade do laço da aula04

---
+++ aula04/conteudo/laco_for.py
@@ -0,0 +1,8 @@
+#Utilizamos o laço for quando sabemos o número de iterações (quantas vezes ele vai percorrer)
+
+frutas = ["maça", "banana", "kiwi", "laranja", "limão", "goiaba"]
+
+for fruta in frutas:
+    print(f"Primeira letra da fruta é: ", fruta[0])
+    print(fruta)
+    
